d126f2b7158d7c95ecfa152a7762e81495ed8921
Jreng7 <jr.waydev@gmail.com>
2024-12-16 20:36:52 -0300

    commit java curso Udemy2

--- carlos_udemy/src/AulasBasicasOfJava.java
+++ carlos_udemy/src/AulasBasicasOfJava.java
@@ -5,8 +5,12 @@ public class AulasBasicasOfJava {
         double numero1 = 240.77;
         int numero2 = (int) numero1;   // Aqui o Java acusa erro, pois, int não pode conter double.
         // porém, se eu colocar na frente da variável o tipo, logo, estou fazendo o "Casting Explícito".
+        // o Java "Trunca" os decimais para armazenar o dado do tipo double em int.
 
         System.out.println(numero1);
         System.out.println(numero2);
+
+        // Strings
+        System.out.println("Olá,\ntudo\nbem"); //     Aqui o "\n" é uma quebra de linha.
     }
 }
