cb7ebb3778a997e45d0a557b414c58c5e65175bc
Jreng7 <jr.waydev@gmail.com>
2024-12-22 16:48:24 -0300

    Fully Qualified Name

---
+++ carlos_udemy/src/aula_classes_metodos_objetos/labs/FullyQualifiedName.java
@@ -0,0 +1,14 @@
+package aula_classes_metodos_objetos.labs;
+
+public class FullyQualifiedName {
+
+  // Fully Qualified Name = Nome completo da Classe. Não consigo acessar a classe externa somente com "Account"
+
+  // Account novaConta = new Account();   <-- Esse código acusa erro!
+  // Fully Qualified Name é usado para ter classes com o mesmo nome, porém, de pacotes diferentes.
+
+  // Porém! O código abaixo é muito verboso e inviável.
+  // aula_classes_metodos_objetos.Account novaConta = new aula_classes_metodos_objetos.Account("a");
+
+
+}
