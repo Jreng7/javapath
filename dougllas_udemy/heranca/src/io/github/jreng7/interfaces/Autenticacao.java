1ffb2fab784934f6c510b67986895222cccedf60
Jreng7 <jr.waydev@gmail.com>
2024-12-25 22:13:10 -0300

    final project empresa

---
+++ dougllas_udemy/heranca/src/io/github/jreng7/interfaces/Autenticacao.java
@@ -0,0 +1,42 @@
+package io.github.jreng7.interfaces;
+
+public interface Autenticacao {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Constante, não é uma propriedade. Deve ser inicializada e já setar o valor para ela.
+  // Valor não pode ser mudado e todos os estados, tanto das constantes quanto dos metodos, são publicos.
+  // String NOME = "Fulano";
+
+  // Metodos abstratos, ou seja, que não tem implementação. Só existe a assinatura do metodo.
+  // Tipo do metodo (void) o nome dele (obterValorX) e os parâmetros ();
+  // void obterValorX();
+
+
+
+}
