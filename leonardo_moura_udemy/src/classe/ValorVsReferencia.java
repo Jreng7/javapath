8694c2b6aceb909fabf70fc4fcbdf9f1ce1a9940
Josué Ribeiro <jr.waydev@gmail.com>
2025-01-03 21:12:13 -0300

    Atribuição por Valor e Atribuição por Referência.3

--- leonardo_moura_udemy/src/classe/ValorVsReferencia.java
+++ leonardo_moura_udemy/src/classe/ValorVsReferencia.java
@@ -3,7 +3,7 @@ package classe;
 public class ValorVsReferencia {
   public static void main(String[] args){
 
-      //
+      // ATRIBUIÇÃO POR VALOR.
       double a = 2;
       double b = a;  //  Atribuição por valor, ou seja, ocorreu uma cópia na memória de a = 2 para b = 2.
       a++;  // Resultado => 3
@@ -11,7 +11,8 @@ public class ValorVsReferencia {
       // Pois o valores são independentes, ou seja, 2 espaços na memória.
 
 
+      // ATRIBUIÇÃO POR REFERÊNCIA.
       Data d1 = new Data(1, 6, 2022);
-      Data d2 = d1; // Atribuição por referência.
+      Data d2 = d1; // Atribuição por referência. (Objeto).
   }
 }
