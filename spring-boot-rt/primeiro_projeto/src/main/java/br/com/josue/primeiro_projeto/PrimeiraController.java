b934179127a4652bc15e6467a5398bb61c8d4715
Jreng7 <jr.waydev@gmail.com>
2025-01-12 20:35:08 -0300

    metodo

--- spring-boot-rt/primeiro_projeto/src/main/java/br/com/josue/primeiro_projeto/PrimeiraController.java
+++ spring-boot-rt/primeiro_projeto/src/main/java/br/com/josue/primeiro_projeto/PrimeiraController.java
@@ -1,8 +1,17 @@
 package br.com.josue.primeiro_projeto;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+
+@RestController
+@RequestMapping("/entrada")
 public class PrimeiraController {
-  
+
+  @GetMapping("/primeirometodo")
+    public String metodoGet(){
+      return "Meu primeiro metodo é GET da API REST";
+    }
 }
+
